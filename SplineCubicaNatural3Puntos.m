7cf1959faa3b3aba918f03afbdbfd7b819baa6b0
Dercont <armando.almengor@utp.ac.pa>
2021-03-11 13:34:00 +0100

    finding index error

--- SplineCubicaNatural3Puntos.m
+++ SplineCubicaNatural3Puntos.m
@@ -5,33 +5,41 @@ function out = SplineCubicaNatural3Puntos(Puntos,incremento_real,tiempo_real)
 % Tiempo Real = tiempo total de la trayectoria.
 
 %Calculo del parámetro de avance de la curva:
-tramos=size(Puntos,1)-1
-inc = Incremento_real / (tiempo_real/tramos)
+out = [];
+tramos = size(Puntos,1) - 1;
+inc = incremento_real / (tiempo_real / 2);
 
 %Cálculo de las derivadas para 3 puntos de partida (Dx,Dy)
-ma_constante = [2 1 0; 1 4 0; 0 1 2];
-ma3_puntos = [3 * [PtosControl(2,1)-PtosControl(1,1)];]
+ma_constante =  [2 1 0; 1 4 1; 0 1 2];
+ma3_puntosx = [3 * (Puntos(2,1) - Puntos(1,1)); 3 * (Puntos(3,1) - Puntos(1,1)); 3 * (Puntos(3,1) - Puntos(2,1))];
+ma3_puntosy = [3 * (Puntos(2,2) - Puntos(1,2)); 3 * (Puntos(3,2) - Puntos(1,2)); 3 * (Puntos(3,2) - Puntos(2,2))];
 
+%Calculando las derivadas
+% dx = inv(ma_constante) * [3 * (Puntos(2,1) - Puntos(1,1)); 3 * (Puntos(3,1) - Puntos(1,1)); 3 * (Puntos(3,1) - Puntos(2,1))];
+% dy = inv(ma_constante) * [3 * (Puntos(2,2) - Puntos(1,2)); 3 * (Puntos(3,2) - Puntos(1,2)); 3 * (Puntos(3,2) - Puntos(2,2))];
+dx = ma_constante * ma3_puntosx;
+dy = ma_constante * ma3_puntosy;
 
-%segmentando las traspuestas
-p0 = Puntos(1,:)';
-p1 = Puntos(2,:)';
-p2 = Puntos(3,:)';
-p3 = Puntos(4,:)';
-
-%Matriz 2x4
-m =  [p0 p1 p2 p3];
-
-%Matriz 4x4
-m2 = [1 -3 3 -1; 0 3 -6 3; 0 0 3 -3; 0 0 0 1];
-
-%Curva de Bézier
-c = [];
-
-% Cálculo de cada punto y de la curva
-    for i=0:incremento_real/tiempo_real:1
-        r = m*m2*[1;i;i^2;i^3];
-        c = [c; r'];
+%Calculando matriz abcd
+for i = 1:2
+    %x
+    ax = Puntos(i,1);
+    bx = dx(1,i);
+    cx = 3 * Puntos((i+1),1) - Puntos(i,1) - 2 * dx(i) - dx(i+1);
+    dx = 2 * Puntos(i,1) - Puntos((i+1),1) + dx(i) + dx(i+1);
+    %y
+    ay = Puntos(i,2);
+    by = dy(i);
+    cy = 3 * Puntos((i+1),2) - Puntos(i,2) - 2 * dy(i) - dy(i+1);
+    dy = 2 * Puntos(i,2) - Puntos((i+1),2) + dy(i) + dy(i+1);
+    
+    ma_abcd = [ax bx cx dx; ay by cy dy;];
+    r = [];
+    for t = 0:inc:1
+        punto_curva = ma_abcd * [1; t; t^2; t^3];
+        r = [r, punto_curva];
     end
-out = c;
+end
+out = r.';
+
 end
