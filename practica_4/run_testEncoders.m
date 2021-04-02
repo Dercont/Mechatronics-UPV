f243fa05a013a5b022af040a41ce42ae6bd940bc
Dercont <armando.almengor@utp.ac.pa>
2021-04-02 21:05:43 +0200

    practica_$

---
+++ practica_4/run_testEncoders.m
@@ -0,0 +1,10 @@
+clear all 
+global giroA_v;
+global giroB_v;
+global tiempo_v; 
+
+giroA_v = 0;
+giroB_v = 0;
+tiempo_v = 0;
+
+ejecutarCodigoNXC testEncoders
---
+++ practica_4/testEncoders.m
@@ -0,0 +1,76 @@
+global giroA_v;
+global giroB_v;
+global tiempo_v;
+
+%Inicialización de variables
+
+giroA_v = 0;
+giroB_v = 0;
+tiempo_v = 0;
+tiempo = 0; 
+Ts = 0.05; %50ms
+
+TextOut(0,LCD_LINE1,'test Encoders'); % Mostramos por pantalla el tipo de test
+TextOut(0,LCD_LINE2,'Presione el boton central para');
+TextOut(0,LCD_LINE3,'comenzar con el test');
+while(~ButtonPressed(BTNCENTER))end % Esperamos a pulsar el boton intro
+ClearScreen(); 
+
+velocidad = 5;
+
+%Test Encoder A
+
+TextOut(0,LCD_LINE1,'Encoder A');
+ResetRotationCount(OUT_A); %Reset encoder
+giro = MotorRotationCount(OUT_A); % grados
+OnFwd(OUT_A, velocidad); % Giro motor A
+
+giroA_v = giro;
+while (giro < 1000)
+    t1 = double(CurrentTick()); % Hasta giro 1000 grados
+    tiempo_v = [tiempo_v tiempo];
+    giro = MotorRotationCount(OUT_A); 
+    NumOut(0, LCD_LINE2, giro);
+    giroA_v = [giroA_v giro];
+    
+    t2 = double(CurrentTick());
+    %Esperar hasta siguiente periodo
+    espera = (max(0, Ts * 1000 - (t2 - t1)));
+    Wait(espera);
+    tiempo = tiempo + Ts;
+end
+
+NumOut(0, LCD_LINE2, giro);
+Off(OUT_A); %Parar motor A
+Wait(1000);
+ResetRotationCount(OUT_A); %Reset encoder y mostrar en pantalla
+giro = MotorRotationCount(OUT_A);
+TextOut(0, LCD_LINE2, 'reseteado');
+NumOut(0, LCD_LINE3, giro);
+Wait(1000);
+ClearScreen();
+
+% Test Encoder C
+TextOut(0, LCD_LINE1, 'Encoder C');
+ResetRotationCount(OUT_C); %Reset encoder
+giro = MotorRotationCount(OUT_C); % grados
+OnFwd(OUT_C, velocidad); % Giro motor C
+
+while (giro < 1000) 
+    NumOut(0, LCD_LINE2, giro);
+    giro = MotorRotationCount(OUT_C);
+end
+
+NumOut(0, LCD_LINE2, giro);
+Off(OUT_C);
+Wait(1000);
+ResetRotationCount(OUT_C); %Reset encoder y mostrar en pantalla
+giro = MotorRotationCount(OUT_C);
+TextOut(0, LCD_LINE2, 'reseteado');
+NumOut(0, LCD_LINE3, giro);
+Wait(1000);
+ClearScreen();
+
+
+
+
