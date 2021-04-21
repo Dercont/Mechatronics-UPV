db9e7586eeb7d9e99d1861ff7cd97c2c8530bfc7
Dercont <armando.almengor@utp.ac.pa>
2021-04-21 13:12:05 +0200

    pra3v5

--- practica_6/ej2.m
+++ practica_6/ej2.m
@@ -9,12 +9,12 @@ function [v_der, v_izq] = Cinematica_Inversa(dot_xc,dot_yc,theta)
 
     %Calculo de las matrices
     cin_inv = [(cos(theta)+(b/e)*sin(theta)) (sin(theta)-(b/e)*cos(theta));...
-          (cos(theta)-(b/e)*sin(theta)) (sin(theta)+(b/e)*cos(theta))];
+          (cos(theta)-(b/e)*sin(theta)) (sin(theta)+(b/e)*cos(theta)) ];
     
     lefrig = 1/2 * cin_inv * p_descentrado;
 
   %Calculo de velocidades
-    v_der = lefrig(1);
-    v_izq = lefrig(2);
+    v_izq = lefrig(1);
+    v_der = lefrig(2);
 
 end
